eafc4a0161101978c442f9cc83d0f4e0f95d71a4
20084190 <20084190@wit.ie>
2019-10-08 11:52:22 +0100

    Second commit

--- src/Utilities/TheatreLinkedList.java
+++ src/Utilities/TheatreLinkedList.java
@@ -21,6 +21,44 @@ public class TheatreLinkedList<T>{
 
     }
 
+    public int length(){
+            var tempNode = head;
+            // Continue loop until end of list when it should return the number
+            for (int i = 0;;i++){
+               if (tempNode == null){
+                   return i;
+               }
+               else{
+                   tempNode = tempNode.next;
+               }
+            }
+    }
+
+    public void removeAll(){
+        // Removing all is very easy, setting head to null will make all of its children disappear;
+       head = null;
+    }
+
+    public void remove(int index) throws  IndexOutOfBoundsException{
+            var tempNode = head;
+            if(index == 0){
+                head = head.next;
+            }
+            for (int i = 0; tempNode != null && index >= 0; i++){
+                // Stop at the node before
+                if (i == index - 1 && tempNode.next != null){
+                    // Set the next of the current node to be the next of the following node
+                   tempNode.next = tempNode.next.next;
+                   return;
+                }
+                else{
+                    tempNode = tempNode.next;
+                }
+            }
+        // If no return happened at this point an exception can be thrown
+        throw new IndexOutOfBoundsException("Index " + index + " is not within the bounds of the list." );
+    }
+
     public T get(int index) throws IndexOutOfBoundsException{
         var tempNode = head;
         // if there is no check for negative index then the loop would go on forever
--- test/TheatreLinkedListTest.java
+++ test/TheatreLinkedListTest.java
@@ -11,10 +11,15 @@ public class TheatreLinkedListTest {
     @BeforeAll
     public static void before() {
         testLinkedList = new TheatreLinkedList<>();
+         testLinkedList.addFront("doing");
+        testLinkedList.addFront("you");
+        testLinkedList.addFront("are");
+        testLinkedList.addFront("how");
     }
 
     @Test
     public void addFrontTest() {
+        testLinkedList.removeAll();
         testLinkedList.addFront("doing");
         testLinkedList.addFront("you");
         testLinkedList.addFront("are");
@@ -24,14 +29,40 @@ public class TheatreLinkedListTest {
     @Test
     public void getTest() {
         assertThrows(IndexOutOfBoundsException.class, () -> {
-            testLinkedList.get(0);
+            testLinkedList.get(-1);
         });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            testLinkedList.get(4);
+        });
+        assertEquals("how", testLinkedList.get(0));
+        assertEquals("doing", testLinkedList.get(3));
+    }
+
+    @Test
+    public void lengthTest(){
+        assertEquals(3, testLinkedList.length());
+        testLinkedList.addFront("Hey");
+        assertEquals(4, testLinkedList.length());
+    }
+
+    @Test
+    public void removeAllTest(){
+        assertEquals(4, testLinkedList.length());
+        testLinkedList.removeAll();
+        assertEquals(0, testLinkedList.length());
+    }
+    @Test
+    public void removeTest(){
         testLinkedList.addFront("doing");
         testLinkedList.addFront("you");
         testLinkedList.addFront("are");
         testLinkedList.addFront("how");
-        assertEquals("how", testLinkedList.get(0));
-        assertEquals("doing", testLinkedList.get(3));
+        assertThrows(IndexOutOfBoundsException.class, () ->{
+            testLinkedList.remove(4);
+        });
+        testLinkedList.remove(2);
+        assertEquals(3, testLinkedList.length());
+        assertEquals("doing", testLinkedList.get(2));
     }
 
     @AfterAll
